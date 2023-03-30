4e63fae6317e09d891604b17cce775aac05d90b1
cihan <kayachnn@gmail.com>
2023-03-30 10:28:03 +0300

    duzeltildi

--- include/Queue.h
+++ include/Queue.h
@@ -6,12 +6,60 @@
 template <typename T>
 class Queue {
 public:
-    Queue();
 
-    void push(const T& val);
-    T pop();
-    int size() const;
-    bool empty() const;
+    Queue() : head(nullptr) {
+
+    }
+
+    Queue(const T& value) : head(new Node{value, nullptr}) {
+    }
+
+
+    void push(const T value){
+
+        std::lock_guard<std::mutex> lock(mutex);
+        Node* node = new Node(value);
+        if (head == nullptr) {
+            head = node;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = node;
+        }
+    }
+    
+    
+    T pop(){
+
+        std::lock_guard<std::mutex> lock(mutex);
+        if (head == nullptr) {
+            throw std::runtime_error("Bos Queue");
+        }
+        T value = head->val;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return value;
+    }
+
+
+    int size() const {
+        std::lock_guard<std::mutex> lock(mutex);
+        int size = 0;
+        Node* current = head;
+        while (current != nullptr) {
+            size++;
+            current = current->next;
+        }
+        return size;
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(mutex);
+        return head == nullptr;
+    }
 
 private:
 struct Node {
